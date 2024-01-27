76103d7dfe6327a0667a441e574a2c464c8859dd
CH3CHOHCH3 <ybc20020730@gmail.com>
2024-01-28 01:32:19 +0800

    feat: clear the .bss section.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -8,5 +8,23 @@ global_asm!(include_str!("entry.S"));
 
 #[no_mangle]
 pub fn start() -> ! {
+    clear_bss();
     loop {}
+}
+
+/**
+ * Qemu with -kernel option will clear the .bss automatically.
+ * However we use -device so we clear it manually.
+ */
+fn clear_bss() {
+    extern "C" {
+        // sbss and ebss are defined in kernel.ld
+        static sbss: usize;
+        static ebss: usize;
+    }
+    unsafe {
+        (sbss..ebss).for_each(|p| {
+            (p as *mut u8).write_volatile(0)
+        });
+    }
 }
